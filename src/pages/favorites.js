4a99baa7331bb0b8b322c26628dd96df9fc6da52
Cheolgu Kim <cgkim@nowcom.com>
2022-05-16 00:01:04 -0700

    page 170

---
+++ src/pages/favorites.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+const Favorites = () => {
+    useEffect(() => {
+        // 문서 제목 업데이트
+        document.title = 'Favorites - Notedly';
+    });
+
+    return (
+        <div>
+            <p>These are my favorites</p>
+        </div>
+    );
+};
+
+export default Favorites;
---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Header from "../components/Header";
+import Navigation from "../components/Navigation";
+
+const Home = () => {
+    return (
+        <div>
+            <p>This is the home page</p>
+        </div>
+    );
+};
+
+export default Home;
