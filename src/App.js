771aa35c3507575c1acb64bd2a18d37336eb56fa
Cheolgu Kim <cgkim@nowcom.com>
2022-05-16 20:09:40 -0700

    page 186

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Import Apollo client library
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
 import GlobalStyle from "./components/GlobalStyle";
 import Pages from "./pages";
 
+// API URI 및 cache 설정
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+
+// Apollo Client 설정
+const client = new ApolloClient({
+    uri,
+    cache,
+    connectToDevTools: true
+});
+
 const App = () => {
     return (
-        <div>
+        <ApolloProvider client={client}>
             <GlobalStyle />
             <Pages />
-        </div>
+        </ApolloProvider>
     )
 };
 
