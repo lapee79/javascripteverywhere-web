7461aaf60fbe2621b445307a0bb0f309900a669e
Cheolgu Kim <cgkim@nowcom.com>
2022-05-26 04:44:57 -0700

    page 214

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import { gql, useMutation } from "@apollo/client";
+import React, { useEffect, useState } from "react";
 import styled from 'styled-components';
 
 import Button from '../components/Button';
@@ -23,17 +24,51 @@ const Form = styled.form`
     }
 `;
 
+const SIGNUP_USER = gql`
+    mutation signUp($email: String!, $username: String!, $password: String!) {
+        signUp(email: $email, username: $username, password: $password)
+    }
+`;
+
 // 나중에 쓰기 위해 props를 포함한 후 component에 전달
 const SignUp = props => {
+    // 기본 양식 상태 설정
+    const [values, setValues] = useState();
+
+    // 사용자가 양식을 채우면 상태 업데이트
+    const onChange = event => {
+        setValues({
+            ...values,
+            [event.target.name]: event.target.value
+        });
+    };
+
     useEffect(() => {
         // 문서 제목 update
         document.title = 'Sign Up - Notedly';
     });
 
+    // mutation hook 추가
+    const [signUp, { loading, error }] = useMutation(SIGNUP_USER, {
+        onCompleted: data => {
+            // mutation이 완료되면 JWT를 logging
+            console.log(data.signUp);
+        }
+    });
+
     return (
         <Wrapper>
             <h2>Sign Up</h2>
-            <Form>
+            {/* User가 Form을 제출하면 데이터를 mutation으로 전달 */}
+            <Form
+                onSubmit={event => {
+                    event.preventDefault();
+                    signUp({
+                        variables: {
+                            ...values
+                        }
+                    });
+                }}>
                 <label htmlFor="username">Username:</label>
                 <input
                     required
@@ -41,6 +76,7 @@ const SignUp = props => {
                     id="username"
                     name="username"
                     placeholder="username"
+                    onChange={onChange}
                 />
                 <label htmlFor="email">Email:</label>
                 <input
@@ -49,6 +85,7 @@ const SignUp = props => {
                     id="email"
                     name="email"
                     placeholder="Email"
+                    onChange={onChange}
                 />
                 <label htmlFor="password">Password:</label>
                 <input
@@ -57,6 +94,7 @@ const SignUp = props => {
                     id="password"
                     name="password"
                     placeholder="Password"
+                    onChange={onChange}
                 />
                 <Button type="submit">Submit</Button>
             </Form>
